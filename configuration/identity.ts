c157489dbf723382fd70bfa340d3705346f51b2c
Elia Movahed <111400092+eliamov@users.noreply.github.com>
2025-02-27 15:48:28 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Elia Movahed`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Top Investment Banks`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
