a3d320648ba0365d6b91113fdf7f4e5ac02c075e
Elia Movahed <111400092+eliamov@users.noreply.github.com>
2025-02-27 16:51:34 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Chat with Jamie`;
 export const MESSAGE_PLACEHOLDER: string = `Jamie is looking at the numbers`;
 export const FOOTER_MESSAGE: string = `Customized AI by Elia Movahed @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear the Chat`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Jamie.AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
