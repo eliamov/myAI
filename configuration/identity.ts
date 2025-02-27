ec92da909a22b96ae540d0393e10fee7954c1264
Elia Movahed <111400092+eliamov@users.noreply.github.com>
2025-02-27 15:49:17 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Elia Movahed`;
 export const OWNER_DESCRIPTION: string = `Top Investment Banks`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Investment Bank Ranker`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
