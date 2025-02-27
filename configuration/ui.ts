83d1f4213a5d2f3715552653174ca21045cff796
Elia Movahed <111400092+eliamov@users.noreply.github.com>
2025-02-27 16:43:09 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Investment Bank Chatbot`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `Chat with Jamie`;
+export const MESSAGE_PLACEHOLDER: string = `Jamie is looking at the numbers`;
 export const FOOTER_MESSAGE: string = `Customized AI by Elia Movahed @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear the Chat`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
