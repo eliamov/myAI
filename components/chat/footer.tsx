bb25180e8d075a9bb89e3bbc731a4dbb209ad256
Elia Movahed <111400092+eliamov@users.noreply.github.com>
2025-03-04 17:28:13 -0500

    Update footer.tsx

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -3,16 +3,15 @@ import Link from "next/link";
 
 export default function ChatFooter() {
   return (
-    <div className="w-full text-xs flex py-3 px-5 border-t transition-all duration-300 
-      bg-gray-100 dark:bg-gray-900 text-gray-500 dark:text-gray-300 border-gray-300 dark:border-gray-700">
+    <div className="w-full text-xs flex pt-2 text-gray-500">
       <div className="flex-grow text-left">
-        {/* Left Pane - Terms of Service Link */}
-        <Link href="/terms" className="hover:underline dark:hover:text-white">
+        {/* Left Pane */}
+        <Link href="/terms" className="hover:underline">
           Terms of Service
         </Link>
       </div>
       <div className="flex-grow text-center">
-        {/* Center Pane - Footer Message */}
+        {/* Center Pane */}
         {FOOTER_MESSAGE}
       </div>
       <div className="flex-grow text-right">
@@ -22,7 +21,6 @@ export default function ChatFooter() {
           href="http://www.ringel.ai"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:underline dark:hover:text-white"
         >
           powered by ringel.AI
         </a>
