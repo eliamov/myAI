dcda9ee108026a48fe47e9d38959c9066f323ff0
Elia Movahed <111400092+eliamov@users.noreply.github.com>
2025-03-04 14:34:03 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,19 +17,24 @@ export default function Chat() {
   } = useApp();
 
   return (
-    <>
+    <div className="h-screen flex flex-col bg-gradient-to-br from-blue-100 via-white to-blue-100">
+      {/* Chat Header */}
       <ChatHeader clearMessages={clearMessages} />
-      <div className="flex justify-center items-center h-screen">
+
+      {/* Chat Messages Section */}
+      <div className="flex flex-1 justify-center items-center">
         <div className="flex flex-col max-w-screen-lg w-full h-full p-5">
           <ChatMessages messages={messages} indicatorState={indicatorState} />
         </div>
       </div>
+
+      {/* Chat Input */}
       <ChatInput
         handleInputChange={handleInputChange}
         handleSubmit={handleSubmit}
         input={input}
         isLoading={isLoading}
       />
-    </>
+    </div>
   );
 }
